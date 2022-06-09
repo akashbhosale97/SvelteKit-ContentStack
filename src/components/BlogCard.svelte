5478f60e1ba08f2a3599480c4347ca1ff40a0297
Akash Bhosale <MY_NAME@example.com>
2022-06-09 12:21:03 +0530

    ...

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -5,10 +5,10 @@
 <a href="/blogs/{blog.uid}" class="link-dark text-decoration-none">
 	<div class="card mb-3" role="button" data-aos="fade-up">
 		<div class="row g-0 align-items-center">
-			<div class="col-md-4">
-				<img src={blog.blog_img.url} class="img-fluid rounded-start" alt="..." />
+			<div class="col-md-12">
+				<img src={blog.blog_img.url} class="img-fluid rounded-start blog_img" alt="..." />
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-12">
 				<div class="card-body">
 					<h5 class="card-title">{blog.blog_title}</h5>
 					<p class="fw-bold my-3">{blog.blog_author}</p>
@@ -20,4 +20,9 @@
 </a>
 
 <style>
+	.blog_img:first-child {
+		width: 100%;
+		height: auto;
+		object-fit: contain;
+	}
 </style>
