0bc153be401adf65c47e188468329608b62c42c3
Akash Bhosale <MY_NAME@example.com>
2022-06-14 12:50:09 +0530

    .

--- src/components/HomeCard.svelte
+++ src/components/HomeCard.svelte
@@ -5,7 +5,7 @@
 <div class="card my-3">
 	<img src={img} class="card-img-top" alt="..." />
 	<div class="card-body">
-		<h5 class="card-title fw-bold homecardtitle">{title}</h5>
+		<p class="card-title h5 fw-bold homecardtitle">{title}</p>
 		<p class="card-text fs-5 my-lg-4 my-sm-5 homecardcontent">
 			{content}
 		</p>
